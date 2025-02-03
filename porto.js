766f2ca79620a0ad88eacf11a9db43175d8973be
dinnayeongx <annisamaulidina12@gmail.com>
2025-02-03 20:37:57 +0700

    Add files via upload

---
+++ porto.js
@@ -0,0 +1,25 @@
+let menuIcon = document.querySelector('#menu');
+let navBar = document.querySelector('.nav-bar');
+let section = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLink.forEach(link => {
+                link.classList.remove('.active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('.active')
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('.active');
+}
